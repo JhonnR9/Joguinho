8e5ee3fdad722d1eb4eb196054f9b8b2bcdeb956
jhone <4.jhonn@gmail.com>
2022-05-27 17:14:14 -0300

    this is an version with a window and  graphics using swig

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,12 +1,19 @@
 import GameConstants.Companion.gameDimension
 import java.awt.Canvas
+import java.awt.Color
+import java.awt.image.BufferedImage
 import javax.swing.JFrame
 
 class Game : Canvas(), Runnable {
     var frame: JFrame = JFrame()
+    lateinit var thread: Thread
+    private var isRunning = true
+
+    private lateinit var image: BufferedImage
 
     init {
         initFrame()
+        image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
     }
 
     private fun initFrame() {
@@ -20,9 +27,61 @@ class Game : Canvas(), Runnable {
 
     }
 
+    @Synchronized
+    fun start() {
+        thread = Thread(this)
+        isRunning = true
+        thread.start()
+    }
+
+    @Synchronized
+    fun stop() {
+    }
+
+    private fun update() {
+
+    }
+
+    private fun render() {
+        var bs = this.bufferStrategy
+        if (bs == null) {
+            this.createBufferStrategy(3)
+            return
+        }
+        var graphics = image.graphics
+        graphics.color = Color.yellow
+        graphics.fillRect(0, 0, width, height)
+        graphics = bs.drawGraphics
+        graphics.drawImage(image, 0, 0, width, height, null)
+        bs.show()
+
+    }
 
     override fun run() {
+        var lastTime = System.nanoTime()
+        val amountOfTicks = 60.0
+        val ns = 1000000000 / amountOfTicks
+        var delta = 0.0
+        var frames = 0
+        var timer = System.currentTimeMillis()
 
+        while (isRunning) {
+            var now = System.nanoTime()
+            delta += (now - lastTime) / ns
+            lastTime = now
+
+            if (delta >= 1) {
+                update()
+                render()
+                frames++
+                delta--
+            }
+            if (System.currentTimeMillis() - timer >= 1000) {
+                println("FPS: $frames")
+                frames = 0
+                timer = System.currentTimeMillis()
+            }
+        }
     }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
 fun main(args: Array<String>) {
-val game = Game()
+    val game = Game()
+    game.start()
 }
