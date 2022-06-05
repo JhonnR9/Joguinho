2ffc314261e06bad3f9be3f6b416412233a7398c
jhone <4.jhonn@gmail.com>
2022-06-05 20:09:03 -0300

    this is a version with map made with Tiled

---
+++ src/main/kotlin/world/Tile.kt
@@ -0,0 +1,11 @@
+package world
+
+import java.awt.Graphics
+import java.awt.Rectangle
+import java.awt.image.BufferedImage
+
+class Tile() {
+    var TileWall = mutableListOf<BufferedImage>()
+    var tileFloor= mutableListOf<BufferedImage>()
+
+}
